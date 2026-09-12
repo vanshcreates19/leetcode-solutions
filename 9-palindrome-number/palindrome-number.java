44d185317d594bd7532ea9e135758d4589a0b1d6
vanshcreates19 <gargvansh1901@gmail.com>
2026-09-12 21:38:24 +0530

    Time: 6 ms (22.11%) | Memory: 46 MB (52.45%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
